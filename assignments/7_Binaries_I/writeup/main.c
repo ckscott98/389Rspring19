56e84f9442418bc4613456171dbf908a01454ab8
root <root@localhost.localdomain>
2019-04-11 13:40:56 -0400

    commented the code

--- assignments/7_Binaries_I/writeup/main.c
+++ assignments/7_Binaries_I/writeup/main.c
@@ -1,11 +1,24 @@
 /*
- * Name: *PUT YOUR NAME HERE*
- * Section: *PUT YOUR SECTION NUMBER HERE*
+ * Name: Cheyenne Scott 
+ * Section: 0201
  *
  * I pledge on my honor that I have not given or received any unauthorized
  * assistance on this assignment or examination.
  *
- * Digital acknowledgement: *PUT YOUR NAME HERE*
+ * Digital acknowledgement: Cheyenne Scott 
  */
 
 /* your code goes here */
+
+int main(){
+	//make both hex values into strings
+	char str1 = 0x1ceb00da;
+	char str2 = 0xfeedface;
+	//xor str2 with str1
+	//xor str1 with str2
+	//xor str1 with str2
+	//makes edi a string
+	//prints eax
+	//makes edi a string
+	//prints eax
+}
