207408f2d9325bd9315e538f3f2b4321f3755464
Cheyenne Scott <ckscott@umd.edu>
2019-04-12 09:02:33 -0400

    fixed the code

--- assignments/7_Binaries_I/writeup/main.c
+++ assignments/7_Binaries_I/writeup/main.c
@@ -10,7 +10,6 @@
 
 /* your code goes here */
 
-<<<<<<< HEAD
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -55,17 +54,4 @@ int main(){
 
 	return 0;
 }
-=======
-int main(){
-	//make both hex values into strings
-	char str1 = 0x1ceb00da;
-	char str2 = 0xfeedface;
-	//xor str2 with str1
-	//xor str1 with str2
-	//xor str1 with str2
-	//makes edi a string
-	//prints eax
-	//makes edi a string
-	//prints eax
-}
->>>>>>> 56e84f9442418bc4613456171dbf908a01454ab8
+
